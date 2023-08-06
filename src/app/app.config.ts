5653eb1d69d15408e0ae7f20239e4283c9b93905
Dario Presutti <dariopresde@protonmail.com>
2023-08-06 11:33:42 +0200

    move application config in main.ts

--- src/app/app.config.ts
+++
@@ -1,6 +0,0 @@
-import { ApplicationConfig } from "@angular/platform-browser";
-
-export const AppConfig: ApplicationConfig = {
-  providers: [
-  ],
-};
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { AppComponent } from './app/app.component';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppConfig } from './app/app.config';
+import { ApplicationConfig, bootstrapApplication } from '@angular/platform-browser';
+import { routes } from './app/app.routes';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+
+const AppConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes, withComponentInputBinding()),
+  ],
+};
 
 bootstrapApplication(AppComponent, AppConfig)
   .catch(err => console.error(err));
