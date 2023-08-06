f4e177c7c6d1a42346f6136ad66e87ceb25d1081
Dario Presutti <dariopresde@protonmail.com>
2023-08-06 15:19:15 +0200

    add routing to /home endpoint

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,16 @@
 import { Routes } from "@angular/router";
 
 export const routes: Routes = [
+  { 
+      path: 'home',
+      loadChildren: () => import('@pages/home/').then(m => m.routes)  
+  },
+  { 
+      path: '', 
+      redirectTo: 'home',
+      pathMatch: 'full' 
+  },
+
 
 ];
 
