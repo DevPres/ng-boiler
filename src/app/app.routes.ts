86609c2b9f3f3e9d5fa6353691040515fa29e94c
Dario Presutti <dariopresde@protonmail.com>
2023-08-07 15:03:19 +0200

    change redirect to home/dahsboard, and handle 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,19 @@
 import { Routes } from "@angular/router";
+import { PageNotFoundComponent } from "@pages/page-not-found.component";
 
 export const routes: Routes = [
+  { 
+    path: '', 
+    redirectTo: 'home',
+    pathMatch: 'full' 
+  },
   { 
       path: 'home',
       loadChildren: () => import('@pages/home/').then(m => m.routes)  
   },
-  { 
-      path: '', 
-      redirectTo: 'home',
-      pathMatch: 'full' 
-  },
-
-
+  {
+      path: "**",
+      loadComponent: () => import('@pages/page-not-found.component').then(m => m.PageNotFoundComponent)
+  }
 ];
 
