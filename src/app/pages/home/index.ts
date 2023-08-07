87d449cd18713c6dc2adfa803f651bc901635c3b
Dario Presutti <dariopresde@protonmail.com>
2023-08-07 15:02:26 +0200

    create sub-routing for dahsboard and info

--- src/app/pages/home/index.ts
+++ src/app/pages/home/index.ts
@@ -1,11 +1,23 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-        { 
-            path: '', 
-            title: 'home', 
-            loadComponent: 
-                () => import('@pages/home/home.component').then(m => m.HomeComponent)
-        }
-    ];
+  { 
+    path: '', 
+    title: 'home', 
+    loadComponent: 
+        () => import('@pages/home/home.component').then(m => m.HomeComponent),
+    children: [
+      {
+        path: 'dashboard', 
+        loadComponent: 
+            () => import('@pages/home/dashboard/dashboard.component').then(m => m.DashboardComponent),
+      },
+      {
+        path: 'info',
+        loadComponent:
+          () => import('@pages/home/info/info.component').then(m => m.InfoComponent),
+      },
+    ]
+  }
+];
 
